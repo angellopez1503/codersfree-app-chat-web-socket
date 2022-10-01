ce578779b95edd0cdaaaa86665cdea0692f28182
Angel Lopez <angellopez15luisa@gmail.com>
2022-10-01 00:27:49 -0500

    20.Filtrar contactos

--- app/Http/Livewire/ChatComponent.php
+++ app/Http/Livewire/ChatComponent.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Contact;
 use Livewire\Component;
 
 class ChatComponent extends Component
 {
+
+    public $search;
+
+    //Propiedad computada
+    public function getContactsProperty(){
+
+        return Contact::where('user_id',auth()->id())
+                ->when($this->search,function($query){
+
+                    $query->where(function($query){
+                        $query->where("name",'like','%'.$this->search.'%')
+                        ->orWhereHas('user',function($query){
+                            $query->where('email','like','%'.$this->search.'%');
+                        });
+                    });
+                })
+                ->get() ?? [];
+    }
+
     public function render()
     {
         return view('livewire.chat-component')->layout('layouts.chat');
